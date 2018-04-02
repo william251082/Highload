927ec1125105e3c1de5220e7d77b007056798b3f
William del Rosario <williamdelrosario@yahoo.com>
2018-04-02 20:09:47 +0200

    Boottrap js features

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,6 +3,7 @@
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
+$this->registerJs("$('[data-toggle=\"popover\"]').popover()");
 ?>
 <div class="site-index">
 
@@ -11,7 +12,9 @@ $this->title = 'My Yii Application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">About Project</a></p>
+        <p>
+            <a class="btn btn-lg btn-success" href="http://www.yiiframework.com" data-toggle="modal" data-target="#about-modal">About Project</a>
+        </p>
     </div>
 
     <div class="body-content">
@@ -73,7 +76,7 @@ $this->title = 'My Yii Application';
                     <a class="btn btn-primary btn-sm" href="http://www.yiiframework.com/extensions/">
                         <span class="glyphicon glyphicon-right-hand" aria-hidden="true"></span>Yii Extensions &raquo;
                     </a>
-                    <a class="btn btn-default btn-sm" href="http://www.yiiframework.com/extensions/">
+                    <a class="btn btn-default btn-sm" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." title = "Author info">
                         <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Author &raquo;
                     </a>
                 </div>
@@ -82,3 +85,24 @@ $this->title = 'My Yii Application';
 
     </div>
 </div>
+
+<div class="modal fade" id="about-modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">About project</h4>
+            </div>
+            <div class="modal-body">
+                <p>About project info</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">
+                    OK
+                </button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->

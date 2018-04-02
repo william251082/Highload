3cf09a5b73275d81bcfd341e93b8a9bf1c81bd2f
William del Rosario <williamdelrosario@yahoo.com>
2018-04-02 19:42:32 +0200

    Adding bootstrap components

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Highload</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">About Project</a></p>
     </div>
 
     <div class="body-content">
@@ -25,17 +25,26 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <div class="btn-group" role="group" aria-label="group1">
+                    <a class="btn btn-primary btn-sm" href="http://www.yiiframework.com/doc/">
+                        <span class="glyphicon glyphicon-right-hand" aria-hidden="true"></span>Read more &raquo;
+                    </a>
+                </div>
             </div>
+
             <div class="col-md-3 col-sm-6">
                 <h2>Heading</h2>
-
+                    <span class="label label-danger">Hot news!</span>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <div class="btn-group" role="group" aria-label="group1">
+                    <a class="btn btn-primary btn-sm" href="http://www.yiiframework.com/forum/">
+                        <span class="glyphicon glyphicon-right-hand" aria-hidden="true"></span>Yii Forum &raquo;
+                    </a>
+                </div>
             </div>
             <div class="clearfix hidden-md hidden-lg"></div>
             <div class="col-md-3 col-sm-6">
@@ -46,7 +55,11 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <div class="btn-group" role="group" aria-label="group1">
+                    <a class="btn btn-primary btn-sm" href="http://www.yiiframework.com/extensions/">
+                        <span class="glyphicon glyphicon-right-hand" aria-hidden="true"></span>Yii Extensions &raquo;
+                    </a>
+                </div>
             </div>
             <div class="col-md-3 col-sm-6">
                 <h2>Heading</h2>
@@ -56,7 +69,14 @@ $this->title = 'My Yii Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <div class=" btn-group" role="group" aria-label="group1">
+                    <a class="btn btn-primary btn-sm" href="http://www.yiiframework.com/extensions/">
+                        <span class="glyphicon glyphicon-right-hand" aria-hidden="true"></span>Yii Extensions &raquo;
+                    </a>
+                    <a class="btn btn-default btn-sm" href="http://www.yiiframework.com/extensions/">
+                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Author &raquo;
+                    </a>
+                </div>
             </div>
         </div>
 

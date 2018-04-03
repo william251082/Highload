7fa61a802405c5843da318a99bb0e1b923c3620b
William del Rosario <williamdelrosario@yahoo.com>
2018-04-03 10:33:11 +0200

    Changing Session strorage to database

--- common/config/main.php
+++ common/config/main.php
@@ -7,7 +7,12 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\DbCache',
+        ],
+        'session' => [
+            'class' => 'yii\web\DbSession',
+            // 'db' => 'mydb',
+            // 'sessionTable' => 'my_session',
         ],
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
